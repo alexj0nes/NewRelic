1c751a556a3a58ac91fbdfa9ea201d8fbc84fd7a
alexj0nes <alex.i.jones@gmail.com>
2017-01-09 09:02:40 -0800

    TEST

---
+++ retryDecorator.py
@@ -0,0 +1,74 @@
+import time
+from functools import wraps
+
+
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        exceptions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck, e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print msg
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
+
+
+#@retry(Exception, tries=4)
+#def test_success(text):
+#    print "Success: ", text
+
+#test_success("it works!")
+
+import random
+
+@retry(Exception, tries=4)
+def test_random(var1,var2,var3):
+    x = var1 + var2
+    x += var3
+    if x < 6:
+        raise Exception("Fail")
+    else:
+         return x
+#        print str(x) + " Success: ", text
+
+for i in range(10):
+    print "trying: " + str(i)
+    try:
+         y = test_random(i,i,i)
+    except:
+         print "failed: " + str(i)
+         continue
+    print "success: " + str(i)
+    print "y: " + str(y)
